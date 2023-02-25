be5ecf44ed2425465944db246ff4d83c04586638
kalash jain <kalashjain54@gmail.com>
2023-02-25 09:44:40 +0530

    shopping app

---
+++ shopping_cart/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import navbar from './components/navbar';
+
+function App() {
+  return (
+   <>
+     <navbar/>
+   </>
+  );
+}
+
+export default App;
